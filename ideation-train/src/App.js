3f4bf8019b6d300a9f7747821170be2ab90cd163
Annie Liu <annielili.liu@gmail.com>
2022-02-18 22:08:42 -0800

    title screen draft

--- ideation-train/src/App.js
+++ ideation-train/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Layout, Button, Typography, Divider } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const { Title, Paragraph, Text, Link } = Typography;
+const { Header, Content, Footer } = Layout;
+
+const App = () => (
+  <div className="App">
+    <Layout className="layout">
+      <Header className="header">
+        {/* figuring this out */}
+        {/* <Typography>
+          <Title level={5}>WELCOME TO THE</Title>
+          <Title>ideation train</Title>
+        </Typography> */}
+      </Header>
+      <Content className="content">
+        <Title level={5}>WELCOME TO THE</Title>
+        <Title>ideation train</Title>
+        <Button type="primary">REVEAL DAILY PROMPT</Button>
+      </Content>
+      <Footer className="footer">
+        <Button type="primary">LEARN HOW TO PLAY</Button>
+      </Footer>
+    </Layout>
+  </div>
+);
 
 export default App;
